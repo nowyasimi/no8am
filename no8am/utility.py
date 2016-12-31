31ec895b19044db5c6002f8c450d74c9d1a23935
Nadeem <nadeemnasimi@gmail.com>
2016-12-31 11:45:38 -0500

    start cleanup of course description getter and integration into module

--- no8am/utility.py
+++ no8am/utility.py
@@ -1,4 +1,14 @@
 from datetime import datetime
+import json
+import io
+import sys
+
+from no8am.metadata import DEPARTMENT_LIST
+from no8am.scraper import *
+
+COURSE_DESCRIPTION_FILENAME = "bucknellCourseDescriptions.json"
+
+DEPARTMENTS = None
 
 
 def get_bucknell_format_semester():
@@ -25,3 +35,109 @@ def get_bucknell_format_semester():
 		return str(course_selection_year) + '01'
 	else:
 		return str(course_selection_year) + '05'
+
+
+# TODO - make this a cron job and write to S3
+
+
+def get_course_numbers_in_department(department_name):
+
+	try:
+		all_courses = Department.process_department_request(department_name)
+	except:
+		return []
+
+	courses_in_department = filter(lambda course: course["deptName"] == department_name, all_courses)
+
+	return map(lambda course: course["deptName"] + " " + course["courseNum"], courses_in_department)
+
+
+def parseAndCurate():
+	courseNums = get_all_course_numbers()
+	print "doing descriptions...."
+	final = []
+	rows = get_all_course_descriptions()
+	print "retrieved all course descriptions, parsing..."
+
+	for x in range(len(rows)):
+		if x % 2 == 0:
+			numAndName = str(rows[x].text).strip()
+			courseNum = numAndName.split(" - ")[0]
+			courseName = numAndName.split(" - ")[1]
+			if courseNum not in courseNums:
+				continue
+			info = rows[x+1].strings
+			count = 0
+			for x in info:
+				if count == 0:
+					count += 1
+				elif count == 1:
+					realInfo = x.strip().replace("\n", " ")
+					break
+			temp = {}
+			temp["courseNum"] = courseNum
+			temp["courseName"] = courseName
+			temp["info"] = realInfo
+			final.append(temp)
+
+	# TODO - add courses that are not in descriptions (without description)
+	# might not be necessary, mostly shows crosslisted courses
+
+	# print "adding missing courses"
+	# courseNumsWithDescriptions = [x["courseNum"] for x in final]
+	# missingCourseNums = [x for x in courseNums if x not in courseNumsWithDescriptions]
+	# print "missing courses:"
+	# print missingCourseNums
+	return final
+
+
+def get_all_course_descriptions():
+	term = get_bucknell_format_semester()
+
+	payload = [("term_in", term), ("call_proc_in","bwckctlg.p_disp_dyn_ctlg"),
+	 ("sel_subj","dummy"), ("sel_levl","dummy"), ("sel_schd","dummy"), ("sel_coll","dummy"),
+	 ("sel_divs","dummy"), ("sel_dept","dummy"), ("sel_attr","dummy")]
+	for x in DEPARTMENTS:
+		payload.append(("sel_subj", x))
+	r = requests.get("https://www.bannerssb.bucknell.edu/ERPPRD/bwckctlg.p_display_courses", params=payload)
+	soup = BeautifulSoup(r.text)
+	table = soup.find_all("table")[3]
+	rows = table.find_all("tr")
+	return rows
+
+
+def get_all_course_numbers():
+	courseNums = []
+	print "Getting course nums..."
+	for x in DEPARTMENTS:
+		print x
+		courseNums += get_course_numbers_in_department(x)
+	return courseNums
+
+
+def generate_course_descriptions():
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
+
+	global DEPARTMENTS
+
+	DEPARTMENTS = [x["short"] for x in DEPARTMENT_LIST]
+
+	return parseAndCurate()
+
+
+if __name__ == "__main__":
+	descriptions = generate_course_descriptions()
+
+	with io.open(COURSE_DESCRIPTION_FILENAME, "w", encoding='utf8') as f:
+		f.write("courseDescriptions = [".decode("utf8"))
+		for x in range(len(descriptions)):
+			data = json.dumps(descriptions[x], ensure_ascii=False).decode('utf8')
+			try:
+				f.write(data)
+			except TypeError:
+				# Decode data to Unicode first
+				f.write(data.decode('utf8'))
+			if x != len(descriptions) - 1:
+				f.write(", ".decode("utf8"))
+		f.write("]".decode("utf8"))
